b696aea4eb7825ea52d6d0f161334a8acef43a97
Andy Wickert <andy@northernwidget.com>
2022-11-14 02:00:47 -0600

    Updated driver for testing

--- run_CosmicErosion.py
+++ run_CosmicErosion.py
@@ -4,10 +4,13 @@ import sys
 sys.path.append('./cosmicErosion')
 from cosmic_erosion import CosmicErosion
 
-ce = CosmicErosion('test_time_series.csv', 5, 0.7)
+ce = CosmicErosion('test_time_series.csv', 5, 0.7,
+                    crn_data='test_synth_data.csv')
 ce.initialize()
 ce.run()
-ce.finalize(show_plot=False, plot_savepath='test2.pdf', csv_savepath='test2.csv')
+ce.evaluate()
+ce.finalize(show_plot=True)
+#ce.finalize(show_plot=False, plot_savepath='test2.pdf', csv_savepath='test2.csv')
 
 """
 # Scratch for future
