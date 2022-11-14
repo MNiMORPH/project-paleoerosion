bc9f38d8ad94c499ba89c9003d525d29432ae0f4
Andy Wickert <andy@northernwidget.com>
2022-11-14 01:23:59 -0600

    data --> model_io It really is a table of model input and output; data are not involved

--- cosmicErosion/cosmic_erosion.py
+++ cosmicErosion/cosmic_erosion.py
@@ -6,15 +6,15 @@ import numpy as np
 
 class CosmicErosion(object):
 
-    def __init__(self, data, P0, attenuation_length ):
+    def __init__(self, model_io, P0, attenuation_length ):
         """
-        :param [data]: Input data table with two columns named:
+        :param [model_io]: Input table with two columns named:
                         (1) Age [yr BP]
                         (2) Erosion rate [mm/yr]
                         The erosion rate is that starting at the associated
                         age and continuing until the subsequent time step.
                         This may be a Pandas DataFrame or a path to a CSV file.
-        :type [data]: pandas.core.frame.DataFrame or csv
+        :type [model_io]: pandas.core.frame.DataFrame or csv
         
         :param [P0]: Surface 10Be production rate
         :type [P0]: float
@@ -23,10 +23,10 @@ class CosmicErosion(object):
         :type [attenuation_length]: float
         """
 
-        if type(data) is str:
-            self.data = pd.read_csv(data)
-        elif type(data) is pandas.core.frame.DataFrame:
-            self.data = data
+        if type(model_io) is str:
+            self.model_io = pd.read_csv(model_io)
+        elif type(model_io) is pandas.core.frame.DataFrame:
+            self.model_io = model_io
         else:
             raise TypeError('Type must be str or Pandas DataFrame')
         
@@ -55,14 +55,14 @@ class CosmicErosion(object):
         """
 
         # Relative surface elevation with time
-        self.dt = self.data['Age [yr BP]'].diff(periods=-1)
-        dz_surf = -self.dt * self.data['Erosion rate [mm/yr]']/1E3
+        self.dt = self.model_io['Age [yr BP]'].diff(periods=-1)
+        dz_surf = -self.dt * self.model_io['Erosion rate [mm/yr]']/1E3
         dz_surf = np.roll(dz_surf, 1)
         dz_surf[0] = 0
-        self.data['Relative surface elevation [m]'] = dz_surf.cumsum()
+        self.model_io['Relative surface elevation [m]'] = dz_surf.cumsum()
         
         # Create column for 10Be concentration
-        self.data['Modeled surface [10Be] [atoms/g]'] = pd.Series(dtype='float')
+        self.model_io['Modeled surface [10Be] [atoms/g]'] = pd.Series(dtype='float')
         
     def update(self, i):
         """
@@ -71,13 +71,13 @@ class CosmicErosion(object):
         :type [i]: int
         """
         # Depths, times, and erosion rates while sample is not yet eroded
-        z_sample = self.data['Relative surface elevation [m]'][i] - \
-                      self.data['Relative surface elevation [m]']
+        z_sample = self.model_io['Relative surface elevation [m]'][i] - \
+                      self.model_io['Relative surface elevation [m]']
         _valid = z_sample < 0 # Only producing CRN when not yet eroded
 
         # Depths
         z_sample = z_sample[_valid]
-        eros_rate = self.data['Erosion rate [mm/yr]'][_valid] / 1E3 # [m/yr]
+        eros_rate = self.model_io['Erosion rate [mm/yr]'][_valid] / 1E3 # [m/yr]
         _dt = self.dt[_valid]
 
         # Piecewise linear integration
@@ -88,13 +88,13 @@ class CosmicErosion(object):
                               )
 
         # Summation
-        self.data.loc[i, 'Modeled surface [10Be] [atoms/g]'] = dC.sum()
+        self.model_io.loc[i, 'Modeled surface [10Be] [atoms/g]'] = dC.sum()
 
     def run(self):
         """
         Solve the 10Be concentration (via update()) for all time steps
         """
-        for i in self.data.index: #range(1, len(data.index)):
+        for i in self.model_io.index:
             self.update(i)
 
     def finalize(self, show_plot=False, plot_savepath=None, csv_savepath=None):
@@ -104,18 +104,18 @@ class CosmicErosion(object):
         if show_plot or plot_savepath:
             self.plot(show_plot, plot_savepath)
         if csv_savepath:
-            self.data.to_csv(csv_savepath)
+            self.model_io.to_csv(csv_savepath)
 
     def plot(self, show=True, savepath=None):
         """
         Plot model output
         """
         from matplotlib import pyplot as plt
-        plt.plot(self.data['Age [yr BP]'][1:]/1000, self.data['Modeled surface [10Be] [atoms/g]'][1:], 'k-', linewidth=2)
+        plt.plot(self.model_io['Age [yr BP]'][1:]/1000, self.model_io['Modeled surface [10Be] [atoms/g]'][1:], 'k-', linewidth=2)
         plt.xlabel('Age [ka]', fontsize=14)
         plt.ylabel('Modeled surface [10Be] [atoms/g]', fontsize=14)
         plt.twinx()
-        plt.step(self.data['Age [yr BP]'][1:]/1000, self.data['Erosion rate [mm/yr]'][:-1], '0.5', linewidth=2)
+        plt.step(self.model_io['Age [yr BP]'][1:]/1000, self.model_io['Erosion rate [mm/yr]'][:-1], '0.5', linewidth=2)
         plt.ylabel('Catchment-averaged erosion rate [mm/yr]', color='.5', fontsize=14)
         plt.tight_layout()
         if savepath:
