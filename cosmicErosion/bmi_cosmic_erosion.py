62083bbd919e5411e5828d8c3c97197829e930ce
Andy Wickert <andy@northernwidget.com>
2022-11-14 01:09:22 -0600

    Empty CSDMS BMI placeholder

---
+++ cosmicErosion/bmi_cosmic_erosion.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+"""
+Basic Model Interface implementation for the 10Be paleoerosion rate model.
+"""
+
+# Currently an empty placeholder
+# See https://github.com/csdms/bmi-example-python/blob/master/heat/bmi_heat.py
+# for information on how to populate this
+
+class BmiCosmicErosion(object):
+
+    def __init__(self):
+        pass
+
