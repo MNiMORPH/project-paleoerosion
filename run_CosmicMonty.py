3b99d071492bf18fe6710d513143f83196e4dcb3
Andy Wickert <andy@northernwidget.com>
2022-11-15 13:36:59 -0600

    Created good fit with test_synth_data

--- run_CosmicMonty.py
+++ run_CosmicMonty.py
@@ -13,33 +13,38 @@ from cosmic_erosion import CosmicErosion, CosmicMonty
 # Bring in to set a range of ages
 _model_io = pd.read_csv('test_time_series.csv')
 
-ages = np.array( _model_io['Age [yr BP]'] )
+#ages = np.array( _model_io['Age [yr BP]'] )
+ages = np.arange(0,22001,1000)
+ages = np.hstack((ages, [140000]))
+ages = ages[::-1]
+
+ages = np.arange(0,40000,1000)
+ages = np.hstack((ages, [140000]))
+ages = ages[::-1]
 
 erosion_rate_min = 0.005 * np.ones(len(ages) - 1)
-erosion_rate_max = 0.075 * np.ones(len(ages) - 1)
+erosion_rate_max = 0.1 * np.ones(len(ages) - 1)
 
 # Last time step constrained by data
-erosion_rate_min[-1] = 0.02
+erosion_rate_min[-1] = 0.005
 erosion_rate_max[-1] = 0.1
 
 # Spin up should be close to initial concentration
 # Maybe make a function to help wtih this
-erosion_rate_min[0] = 0.05
-erosion_rate_max[0] = 0.07
-erosion_rate_min[1] = 0.05
-erosion_rate_max[2] = 0.07
+#erosion_rate_min[0] = 0.065
+#erosion_rate_max[0] = 0.07
 
 cm = CosmicMonty(5, 0.7, ages, 'test_synth_data.csv')
 
 cm.initialize_minmax_mode(erosion_rate_min, erosion_rate_max)
 cm.initialize_ages(ages)
 cm.initialize_output( csv_dir_1sigma = 
-                      '/home/awickert/Desktop/CosmicMontyTest03/1_sigma',
+                      '/home/awickert/Desktop/CosmicMontyTest04/1_sigma',
                         csv_dir_2sigma = 
-                      '/home/awickert/Desktop/CosmicMontyTest03/2_sigma',
+                      '/home/awickert/Desktop/CosmicMontyTest04/2_sigma',
                      )
 
-cm.mcloop(5000, verbose=True)
+cm.mcloop(50000, verbose=True)
 
 
 
