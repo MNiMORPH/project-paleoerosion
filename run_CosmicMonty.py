7b35b1b02100ee7bb2653ca4b1123d95e802e3a4
Andy Wickert <andy@northernwidget.com>
2022-11-15 01:19:13 -0600

    Inputs during first success

--- run_CosmicMonty.py
+++ run_CosmicMonty.py
@@ -15,12 +15,17 @@ _model_io = pd.read_csv('test_time_series.csv')
 
 ages = np.array( _model_io['Age [yr BP]'] )
 
-erosion_rate_min = 0.01 * np.ones(len(ages) - 1)
-erosion_rate_max = 0.2 * np.ones(len(ages) - 1)
+erosion_rate_min = 0.005 * np.ones(len(ages) - 1)
+erosion_rate_max = 0.075 * np.ones(len(ages) - 1)
 
 # Last time step constrained by data
-erosion_rate_min[-1] = 0.3
-erosion_rate_max[-1] = 0.5
+erosion_rate_min[-1] = 0.02
+erosion_rate_max[-1] = 0.1
+
+# Spin up should be close to initial concentration
+# Maybe make a function to help wtih this
+erosion_rate_min[0] = 0.066
+erosion_rate_max[0] = 0.068
 
 cm = CosmicMonty(5, 0.7, ages, 'test_synth_data.csv')
 
@@ -29,7 +34,7 @@ cm.initialize_ages(ages)
 cm.initialize_output( csv_dir_2sigma = 
                               '/home/awickert/Desktop/CosmicMonty2sigmaTest' )
 
-cm.mcloop(2)
+cm.mcloop(5000, verbose=True)
 
 
 
